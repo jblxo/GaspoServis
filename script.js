b0a82971ba4fac30d1033f4b230bb5410d96f3bf
jblxo <hliba.ondra@live.com>
2018-08-12 15:48:19 +0200

    Project Reset

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleNavbar = () => {
-  const navs = document.querySelectorAll('.navbar-items');
-
-  navs.forEach(nav => {
-    nav.classList.toggle('navbar-toggle-show');
-  });
-}
-
-document.querySelector('.navbar-toggle').addEventListener('click', toggleNavbar);
