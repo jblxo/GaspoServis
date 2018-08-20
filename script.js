e54305e0d122d643ffb3e4de7f512c4e9e351ca5
Ondřej Hliba <hliba.ondra@live.com>
2018-08-20 10:04:38 +0200

    Add Feature Section

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const scrollToSectionAbout = () => {
     window.scroll({
-        top: 3500,
+        top: 850,
         left: 0,
         behavior: 'smooth'
     });
 }
+
+window.addEventListener('scroll', (e) => {
+    if(window.scrollY >= 500) {
+        return document.querySelector('#arrow').classList.remove('arrow--animated');
+    }
+    return document.querySelector('#arrow').classList.add('arrow--animated');
+})
 
