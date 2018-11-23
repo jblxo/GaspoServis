ca1111d10a2a1b9feba18c0388ce41479c4002be
Ondřej Hliba <hliba.ondra@live.com>
2018-11-23 10:56:47 +0100

    Minor Tweaks

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const scrollToSectionAbout = () => {
-    window.scroll({
-        top: 800,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
+  window.scroll({
+    top: 800,
+    left: 0,
+    behavior: 'smooth'
+  });
+};
 
 const scrollToSectionContact = () => {
-    window.scroll({
-        top: 2300,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
+  window.scroll({
+    top: 2300,
+    left: 0,
+    behavior: 'smooth'
+  });
+};
 
-window.addEventListener('scroll', (e) => {
-    if(window.scrollY >= 500) {
-        return document.querySelector('#arrow').classList.remove('arrow--animated');
-    }
-    return document.querySelector('#arrow').classList.add('arrow--animated');
+window.addEventListener('scroll', e => {
+  if (window.scrollY >= 500) {
+    return document.querySelector('#arrow').classList.remove('arrow--animated');
+  }
+  return document.querySelector('#arrow').classList.add('arrow--animated');
 });
 
 const currentYear = new Date().getFullYear().toString();
